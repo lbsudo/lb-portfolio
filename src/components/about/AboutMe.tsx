643eaa0635f515dd107293cb50b2ad62ac8abe58
lbsudo <lbsudo100@gmail.com>
2023-11-29 03:55:16 -0800

    typography optimization

--- src/components/about/AboutMe.tsx
+++ src/components/about/AboutMe.tsx
@@ -8,8 +8,8 @@ export default function AboutMe() {
         <p className='text-2xl pt-4 font-bold'>About Me</p>
         <div className='flex flex-col justify-center items-center pt-2'>
           <div className="gradient-line my-2 w-full z-20"></div>
-          <p className='text-left w-full pb-2'>Hello! I'm <span className='font-bold'>Lawrence Brown.</span></p>
-          <p className='text-left w-full pb-2'>Full-Stack Software developer from San Bernadino, California. I'm a JavaScript junkie that's enthusiastic about coding and making software as a whole. Proficient in multiple programming languages, with a focused on web development and a solid foundation in software development and programming principles. Eager to join and contribute to a team as well as push open-source projects forward.</p>
+          <p className='text-left w-full pb-2'>Hello! I&apos;m <span className='font-bold'>Lawrence Brown.</span></p>
+          <p className='text-left w-full pb-2'>Full-Stack Software developer from San Bernadino, California. I&apos;m a JavaScript junkie that's enthusiastic about coding and making software as a whole. Proficient in multiple programming languages, with a focused on web development and a solid foundation in software development and programming principles. Eager to join and contribute to a team as well as push open-source projects forward.</p>
           <p className='text-left w-full'>Besides Programming, I also love cars, gaming, and martial arts.</p>
           <div className='pt-4 flex justify-between flex-col w-full'>
             <Infotags title="Age:" value="24" />
