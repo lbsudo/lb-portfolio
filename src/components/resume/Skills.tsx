968d574dbd9decdb661b5d03d04c8b6a0192003c
lbsudo <lbsudo100@gmail.com>
2023-12-18 21:05:24 -0800

    Changes to skill page

--- src/components/resume/Skills.tsx
+++ src/components/resume/Skills.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { FaGitAlt, FaLinux, FaWordpressSimple } from 'react-icons/fa';
-import { FaHtml5, FaCss3Alt, FaPython, FaPhp, FaReact, FaWindows, FaGithub, FaNodeJs } from "react-icons/fa6";
-import { SiJavascript, SiTypescript, SiWordpress, SiVisualstudiocode, SiSublimetext, SiGraphql, SiCplusplus, SiGnubash, SiAmazonaws, SiPlanetscale, SiNodedotjs, SiNextdotjs, SiSvelte, SiPhp, SiPython, SiSolid, SiLua, SiAstro, SiTailwindcss, SiNextui, SiMysql, SiPostgresql, SiMongodb, SiNetlify, SiVercel, SiArchlinux, SiNeovim, SiExpress } from "react-icons/si";
-
+import { SiJavascript, SiTypescript, SiPhp, SiPython, SiDart, SiLua, SiGnubash, SiGit } from "react-icons/si";
+import { SiHtml5, SiCss3, SiSass, SiAstro, SiSolid, SiReact, SiTailwindcss, SiBootstrap, SiRedux } from "react-icons/si";
+import { SiNextdotjs, SiNodedotjs, SiExpress, SiMysql, SiPostgresql, SiPlanetscale, SiMongodb, SiGraphql, SiAmazonaws } from 'react-icons/si'
+import { SiLinux, SiArchlinux, SiWindows, SiGooglecloud, SiAdobe, SiNeovim, SiVisualstudiocode, SiGithub } from "react-icons/si";
 export default function Skills() {
   return (
     <>
@@ -10,61 +10,61 @@ export default function Skills() {
         <p className='text-2xl pt-4 font-bold'>Skills</p>
         <div className='flex flex-col text-center justify-center items-center pt-2'>
           <div className="gradient-line my-2 w-full z-20"></div>
-          <p className='font-semibold text-xl'>Languages</p>
+          <p className='font-semibold text-xl'>Core</p>
           <div className="grid grid-cols-3 grid-rows-2 gap-4">
-            <span className=''><FaHtml5 className='p-3 text-6xl text-white' /></span>
-            <span className=''><FaCss3Alt className='p-3 text-6xl text-white' /></span>
             <span className=''><SiJavascript className='p-3 text-6xl text-white' /></span>
             <span className=''><SiTypescript className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiPython className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiLua className='p-3 text-6xl text-white' /></span>
             <span className=''><SiPhp className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiCplusplus className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiPython className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiDart className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiLua className='p-3 text-6xl text-white' /></span>
             <span className=''><SiGnubash className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiGit className='p-3 text-6xl text-white' /></span>
           </div>
         </div>
         <div className='flex flex-col text-center justify-center items-center '>
           <div className="gradient-line my-2 w-full z-20"></div>
-          <p className='font-semibold text-xl'>Frameworks, libraries, and tools</p>
+          <p className='font-semibold text-xl'>Front-End</p>
           <div className="grid grid-cols-3 grid-rows-3 gap-4">
-            <span className=''><FaReact className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiNextdotjs className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiSolid className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiHtml5 className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiCss3 className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiReact className='p-3 text-6xl text-white' /></span>
             <span className=''><SiAstro className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiSolid className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiSass className='p-3 text-6xl text-white' /></span>
             <span className=''><SiTailwindcss className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiNextui className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiSvelte className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiExpress className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiNodedotjs className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiBootstrap className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiRedux className='p-3 text-6xl text-white' /></span>
           </div>
         </div>
         <div className='flex flex-col text-center justify-center items-center pt-2'>
           <div className="gradient-line my-2 w-full z-20"></div>
-          <p className='font-semibold text-xl'>Database and CMS platforms</p>
+          <p className='font-semibold text-xl'>Back-end</p>
           <div className="grid grid-cols-3 grid-rows-3 gap-4">
-            <span className=''><SiPlanetscale className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiNextdotjs className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiNodedotjs className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiExpress className='p-3 text-6xl text-white' /></span>
             <span className=''><SiMysql className='p-3 text-6xl text-white' /></span>
             <span className=''><SiPostgresql className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiPlanetscale className='p-3 text-6xl text-white' /></span>
             <span className=''><SiMongodb className='p-3 text-6xl text-white' /></span>
             <span className=''><SiGraphql className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiWordpress className='p-3 text-6xl text-white' /></span>
             <span className=''><SiAmazonaws className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiNetlify className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiVercel className='p-3 text-6xl text-white' /></span>
+
           </div>
         </div>
         <div className='flex flex-col text-center justify-center items-center pt-2'>
           <div className="gradient-line my-2 w-full z-20"></div>
-          <p className='font-semibold text-xl'>Software/Other</p>
+          <p className='font-semibold text-xl'>Tools</p>
           <div className="grid grid-cols-3 grid-rows-3 gap-4">
-            <span className=''><FaLinux className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiLinux className='p-3 text-6xl text-white' /></span>
             <span className=''><SiArchlinux className='p-3 text-6xl text-white' /></span>
-            <span className=''><FaWindows className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiWindows className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiGooglecloud className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiAdobe className='p-3 text-6xl text-white' /></span>
             <span className=''><SiNeovim className='p-3 text-6xl text-white' /></span>
             <span className=''><SiVisualstudiocode className='p-3 text-6xl text-white' /></span>
-            <span className=''><SiSublimetext className='p-3 text-6xl text-white' /></span>
-            <span className=''><FaGithub className='p-3 text-6xl text-white' /></span>
-            <span className=''><FaGitAlt className='p-3 text-6xl text-white' /></span>
+            <span className=''><SiGithub className='p-3 text-6xl text-white' /></span>
           </div>
         </div>
 
